f5c595c9c586698d0f377133b47bcfef4928f464
John Puddephatt <john@jdp.org.uk>
2022-07-24 21:28:12 +0100

    Update colours to work with Nova 4
    Nova 4 uses Tailwind in JIT mode. As a result of this only colours used by Nova are present in the stylesheet.
    
    This commit updates the colours used by this tool to ones that are present in the Nova stylesheet.

--- src/LogsService.php
+++ src/LogsService.php
@@ -15,15 +15,15 @@ class LogsService
     private static string $file;
 
     private static array $levels_classes = [
-        'debug'     => 'text-blue',
-        'info'      => 'text-blue',
-        'notice'    => 'text-blue',
-        'warning'   => 'text-orange',
-        'error'     => 'text-red',
-        'critical'  => 'text-red',
-        'alert'     => 'text-red',
-        'emergency' => 'text-red',
-        'processed' => 'text-blue',
+        'debug'     => 'text-blue-500',
+        'info'      => 'text-blue-500',
+        'notice'    => 'text-blue-500',
+        'warning'   => 'text-yellow-500',
+        'error'     => 'text-red-500',
+        'critical'  => 'text-red-500',
+        'alert'     => 'text-red-500',
+        'emergency' => 'text-red-500',
+        'processed' => 'text-blue-500',
     ];
 
     private static array $levels_imgs = [
